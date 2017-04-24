aef382c50b4763dec420c42d4d06a8206eab2bde
James H. Hill <hilljh82@gmail.com>
2017-04-24 01:15:27 -0400

    Added prefixes to local storage

--- addon/services/gatekeeper.js
+++ addon/services/gatekeeper.js
@@ -1,8 +1,8 @@
 import Ember from 'ember';
 
-const STORAGE_USER_TOKEN = 'storage.user_token';
-const STORAGE_CURRENT_USER = 'storage.current_user';
-const STORAGE_CLIENT_TOKEN = 'storage.client_token';
+const STORAGE_USER_TOKEN = 'storage.gatekeeper::userToken';
+const STORAGE_CURRENT_USER = 'storage.gatekeeper::currentUser';
+const STORAGE_CLIENT_TOKEN = 'storage.gatekeeper::clientToken';
 
 export default Ember.Service.extend({
   /// Reference to local storage.
