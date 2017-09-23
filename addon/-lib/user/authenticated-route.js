d53a8925194699e2225ab8f494f093553832199b
James H. Hill <hilljh82@gmail.com>
2017-09-23 17:19:07 -0400

    Check sign-in state before/after model; listen for signedOut event

--- addon/-lib/user/authenticated-route.js
+++ addon/-lib/user/authenticated-route.js
@@ -2,12 +2,35 @@ import Ember from 'ember';
 
 export default Ember.Route.extend ({
   gatekeeper: Ember.inject.service (),
-
   currentUser: Ember.computed.readOnly ('gatekeeper.currentUser'),
+
+  init () {
+    this._super (...arguments);
+
+    this.get ('gatekeeper').on ('signedOut', this, 'didSignOut');
+  },
+
+  destroy () {
+    this._super (...arguments);
+
+    this.get ('gatekeeper').off ('signedOut', this, 'didSignOut');
+  },
 
   beforeModel (transition) {
     this._super (...arguments);
+    this._checkSignedIn (transition);
+  },
 
+  afterModel (model, transition) {
+    this._super (...arguments);
+    this._checkSignedIn (transition);
+  },
+
+  didSignOut () {
+
+  },
+
+  _checkSignedIn (transition) {
     let isSignedIn = this.get ('gatekeeper.isSignedIn');
 
     if (!isSignedIn) {
