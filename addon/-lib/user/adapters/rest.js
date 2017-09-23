e074c6bc2f9ced0dd04df0931cf31a4c070da645
James H. Hill <hilljh82@gmail.com>
2017-09-23 11:22:49 -0400

    REST adapter forces sign out on 403 unknown_token

--- addon/-lib/user/adapters/rest.js
+++ addon/-lib/user/adapters/rest.js
@@ -12,5 +12,21 @@ export default DS.RESTAdapter.extend({
 
   headers: Ember.computed ('gatekeeper.userToken', function () {
     return { Authorization: `Bearer ${this.get ('gatekeeper.userToken.access_token')}` };
-  })
+  }),
+
+  handleResponse (status, headers, payload, requestData) {
+    switch (status) {
+      case 403:
+        switch (payload.errors.code) {
+          case 'unknown_token':
+            // The token we are using is invalid. We need to force the service to
+            // sign out the current user.
+            this.get ('gatekeeper').forceSignOut ();
+            break;
+        }
+        break;
+    }
+
+    return this._super (...arguments);
+  }
 });
