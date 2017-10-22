581f11cde00f28857e149d9fa300557db20d92af
James H. Hill <hilljh82@gmail.com>
2017-10-22 00:44:23 -0400

    Added changePassword api action

--- addon/models/account.js
+++ addon/models/account.js
@@ -1,7 +1,12 @@
 import DS from 'ember-data';
+import { memberAction, collectionAction } from 'ember-api-actions';
 
 export default DS.Model.extend({
   email: DS.attr ('string'),
+
   username: DS.attr ('string'),
-  password: DS.attr ('string')
+
+  password: DS.attr ('string'),
+
+  changePassword: memberAction ({ path: 'password', type: 'post', urlType: 'findRecord'})
 });
