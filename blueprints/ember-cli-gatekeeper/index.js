6f8dd44382e3cd3bd552c1efe3aff4c13617fb4a
James H. Hill <hilljh82@gmail.com>
2017-11-04 16:11:12 -0400

    Updated ember-cli-mdl

--- blueprints/ember-cli-gatekeeper/index.js
+++ blueprints/ember-cli-gatekeeper/index.js
@@ -5,7 +5,7 @@ module.exports = {
     return this.addAddonsToProject ({
       packages: [
         {name: '@onehilltech/ember-cli-storage', target: '^0.2.1'},
-        {name: 'ember-cli-mdl', target: '^0.14.0'},
+        {name: 'ember-cli-mdl', target: '^0.15.0'},
         {name: 'ember-cli-google', target: '^0.4.2'},
         {name: 'ember-api-actions', target: '^0.1.7'},
       ]
