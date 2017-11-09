24af3638687ed611b64eb49afe232f6c6ced9694
James H. Hill <hilljh82@gmail.com>
2017-11-09 10:02:39 -0500

    Added was named incorrectly

--- blueprints/ember-cli-gatekeeper/index.js
+++ blueprints/ember-cli-gatekeeper/index.js
@@ -6,7 +6,7 @@ module.exports = {
       packages: [
         {name: '@onehilltech/ember-cli-storage', target: '^0.2.1'},
         {name: 'ember-cli-mdl', target: '^0.18.2'},
-        {name: 'ember-cli-recaptcha', target: '^1.0.0'},
+        {name: 'ember-cli-google-recaptcha', target: '^1.0.0'},
         {name: 'ember-api-actions', target: '^0.1.7'},
       ]
     });
