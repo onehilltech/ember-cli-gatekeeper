f33331a9acef92d44f60cf6975e15b87fd11e085
James H. Hill <hilljh82@gmail.com>
2017-11-27 07:41:26 -0500

    User account was missing enabled attribute

--- addon/models/account.js
+++ addon/models/account.js
@@ -2,11 +2,19 @@ import DS from 'ember-data';
 import { memberAction, collectionAction } from 'ember-api-actions';
 
 export default DS.Model.extend({
+  /// Email address for the account.
   email: DS.attr ('string'),
 
+  /// Username of the account.
   username: DS.attr ('string'),
 
+  /// Password for the account. The password is only used with the account is being
+  /// created.
   password: DS.attr ('string'),
 
+  /// The enabled state of the account.
+  enabled: DS.attr ('boolean'),
+
+  /// Change the password for the account.
   changePassword: memberAction ({ path: 'password', type: 'post', urlType: 'findRecord'})
 });
