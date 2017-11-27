dc6b630d1aad792ca60a1172e24e4aa4f738712a
James H. Hill <hilljh82@gmail.com>
2017-11-26 22:37:21 -0500

    Patch mdl

--- blueprints/ember-cli-gatekeeper/index.js
+++ blueprints/ember-cli-gatekeeper/index.js
@@ -5,7 +5,7 @@ module.exports = {
     return this.addAddonsToProject ({
       packages: [
         {name: '@onehilltech/ember-cli-storage', target: '^0.2.1'},
-        {name: 'ember-cli-mdl', target: '^0.18.8'},
+        {name: 'ember-cli-mdl', target: '^0.18.9'},
         {name: 'ember-cli-google-recaptcha', target: '^1.0.0'},
         {name: 'ember-api-actions', target: '^0.1.7'},
       ]
