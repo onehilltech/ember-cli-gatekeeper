3e991c705dd040ce1a768274a77cdf46e7ae1d14
James H. Hill <hilljh82@gmail.com>
2017-11-30 17:21:02 -0500

    Handle accountCreated action

--- tests/dummy/app/controllers/sign-up.js
+++ tests/dummy/app/controllers/sign-up.js
@@ -2,13 +2,9 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
   actions: {
-    createAccount () {
-      let {username, password, email} = this.getProperties (['username', 'password', 'email']);
-      let account = this.get ('store').createRecord ('account', {username, password, email});
-
-      account.save ().then (() => {
-
-      });
+    accountCreated (account) {
+      let message = `Created account for ${account.get ('username')}`;
+      this.set ('messageToUser', message);
     }
   }
 });
