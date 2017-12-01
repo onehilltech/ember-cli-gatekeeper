f3e6c421239896dacef41ce228d231e900df145c
James H. Hill <hilljh82@gmail.com>
2017-11-30 20:03:13 -0500

    Export CreateAccountComponent from module

--- addon/index.js
+++ addon/index.js
@@ -3,8 +3,10 @@ import {default as SignInComponent} from './-lib/components/sign-in';
 import {default as SignInMixin} from './-lib/mixins/sign-in';
 import {default as SignInRoute} from './-lib/routes/sign-in';
 import {default as User} from './-lib/user';
+import {default as CreateAccountComponent} from './components/gatekeeper-create-account';
 
 export default {
+  CreateAccountComponent,
   User,
   SignInComponent,
   SignInController,
