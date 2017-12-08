7edff98f8fdbbc435b216f889a66b5f89c04119c
James H. Hill <hilljh82@gmail.com>
2017-12-08 01:09:56 -0500

    Only serialized changed attributes

--- addon/serializers/account.js
+++ addon/serializers/account.js
@@ -6,5 +6,15 @@ export default DS.RESTSerializer.extend({
 
   keyForAttribute (key) {
     return Ember.String.underscore (key);
+  },
+
+  serializeAttribute (snapshot, json, key, attributes) {
+    // Do not serialize any attributes that are null.
+    const attrs = snapshot.changedAttributes();
+    const changed = attrs[key];
+
+    if (changed && snapshot.attr (key)) {
+      this._super (snapshot, json, key, attributes);
+    }
   }
 });
