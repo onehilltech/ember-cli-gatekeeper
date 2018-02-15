8fb0958dbc056b59e053be55938d46596ba6fb7c
James H. Hill <hilljh82@gmail.com>
2018-02-15 01:00:33 -0500

    Export the sign in vertical component

--- addon/index.js
+++ addon/index.js
@@ -3,11 +3,13 @@ import {default as SignInComponent} from './-lib/components/sign-in';
 import {default as SignInRoute} from './-lib/routes/sign-in';
 import {default as User} from './-lib/user';
 import {default as CreateAccountComponent} from './components/gatekeeper-create-account';
+import {default as SignInVerticalComponent} from './components/gatekeeper-sign-in-vertical';
 
 export default {
   CreateAccountComponent,
   User,
   SignInComponent,
+  SignInVerticalComponent,
   SignInController,
   SignInRoute,
 };
