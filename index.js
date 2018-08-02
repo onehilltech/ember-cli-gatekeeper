f5a4ae96010c39138f71fac20fdab89ea8a0a7d7
James H. Hill <hilljh82@gmail.com>
2018-08-02 00:32:36 -0400

    Fixed include issued with jrsasign

--- index.js
+++ index.js
@@ -10,12 +10,6 @@ module.exports = {
 
   included: function (app) {
     this._super.included.apply (this, arguments);
-
-    if (app.isProduction) {
-      app.import (app.bowerDirectory + '/jsrsasign/jsrsasign-all-min.js');
-    }
-    else {
-      app.import (app.bowerDirectory + '/kjur-jsrsasign/jsrsasign-all-min.js');
-    }
+    app.import (app.bowerDirectory + '/jsrsasign/jsrsasign-all-min.js');
   }
 };
