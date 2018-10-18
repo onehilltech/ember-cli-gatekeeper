2eb0f7873c3524d01b33a7366ea54290643e81e8
James H. Hill <hilljh82@gmail.com>
2018-10-17 21:58:20 -0400

    Switched back to ember-browserfy for micromatch

--- index.js
+++ index.js
@@ -10,22 +10,5 @@ module.exports = {
     this._super (...arguments);
 
     app.import ('node_modules/jsrsasign/lib/jsrsasign-all-min.js');
-
-    // We are using ember-auto-import to import micromatch into the ember application.
-    // The module uses 'fs', but the browser application really cannot use it in the
-    // browser. We need to inform ember-auto-import to disregard the fs module, but
-    // we do not want users of this add-on to manually update their application configuration.
-    // Let's update the application options behind the scene so everything compiles.
-
-    merge (app.options, {
-      autoImport: {
-        webpack: {
-          node: {
-            fs: 'empty'
-          }
-        }
-      }
-    });
-
   },
 };
