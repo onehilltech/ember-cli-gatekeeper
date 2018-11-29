9fb07a39c22b1505b17801c257fa0b7e392bd6e3
James H. Hill <hilljh82@gmail.com>
2018-11-28 20:46:25 -0500

    doc: Added custom explaining use of signInOptions in custom example

--- tests/dummy/app/controllers/sign-in/custom.js
+++ tests/dummy/app/controllers/sign-in/custom.js
@@ -7,6 +7,11 @@ export default Controller.extend (Completed, {
   init () {
     this._super (...arguments);
 
+    // The signInOptions is not required unless you are overriding the default
+    // sign in options defined in config/environment.js. We are only doing it
+    // here because we need to test different version of the sign in in the dummy
+    // test application
+
     this.set ('signInOptions', {
       client_id: '58ed90e1105aee00001e429f',
       client_secret: 'gatekeeper-android'
