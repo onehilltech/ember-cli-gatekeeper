7695f5271ff996299761929ba447265ccd93c574
James H. Hill <hilljh82@users.noreply.github.com>
2018-11-28 21:35:33 -0500

    Update custom-recaptcha.js

--- tests/dummy/app/controllers/sign-in/custom-recaptcha.js
+++ tests/dummy/app/controllers/sign-in/custom-recaptcha.js
@@ -7,6 +7,9 @@ export default Controller.extend (Completed, {
   init () {
     this._super (...arguments);
 
+    // The signInOptions is not required here. We are using it here because 
+    // we are override the default configuration in config/environment.js.
+    
     this.set ('signInOptions', {
       client_id: '5a206991201dc8357e45d174'
     });
