d4c3256665433a37a936870caac2237bec3745ee
James H. Hill <hilljh82@gmail.com>
2018-12-19 02:14:12 -0500

    fix: Updated adapter to 3.3.0

--- addon/adapters/account.js
+++ addon/adapters/account.js
@@ -1,5 +1,8 @@
 import RESTAdapter from '../-lib/user/adapters/rest';
-import Ember from 'ember';
+
+import { isPresent, isEmpty } from '@ember/utils';
+import { getWithDefault } from '@ember/object';
+import { readOnly } from '@ember/object/computed';
 
 /**
  * @class AccountRESTAdapter
@@ -7,24 +10,20 @@ import Ember from 'ember';
  * The RESTAdapter for the account model.
  */
 export default RESTAdapter.extend({
+  /// Get the host from the base url for the client.
+  host: readOnly ('session.gatekeeper.baseUrl'),
+
   createRecord (store, type, snapshot) {
     let _super = this._super;
     let adapter = this;
-    let adapterOptions = snapshot.adapterOptions;
+    let { adapterOptions } = snapshot;
     let opts = {};
 
-    if (Ember.isPresent (adapterOptions)) {
-      const {useUserToken} = adapterOptions;
+    if (isPresent (Object.keys (adapterOptions))) {
+      Object.assign (opts, adapterOptions);
 
-      // If we are to use the user token, then ter
-      if (useUserToken) {
-        return this._super (...arguments);
-      }
-
-      let recaptcha = adapterOptions.recaptcha;
-
-      if (Ember.isPresent (recaptcha)) {
-        opts.recaptcha = recaptcha;
+      if (opts.signIn) {
+        delete opts.signIn;
       }
     }
 
@@ -33,84 +32,66 @@ export default RESTAdapter.extend({
     })
   },
 
-  urlForRequest (params) {
-    let { type, snapshot, requestType, query } = params;
+  urlForCreateRecord (modelName, snapshot) {
+    let url = this._super (...arguments);
 
-    // type and id are not passed from updateRecord and deleteRecord, hence they
-    // are defined if not set
-    type = type || (snapshot && snapshot.type);
+    let signIn = getWithDefault (snapshot, 'adapterOptions.signIn', false);
 
-    switch (requestType) {
-      case 'createRecord': {
-        let url = this._super (...arguments);
-        let signIn = Ember.getWithDefault (snapshot, 'adapterOptions.signIn', false);
-
-        if (signIn) {
-          url += '?login=true';
-        }
-
-        return url;
-      }
-
-      case 'queryRecord':
-        if (Ember.isEmpty (Object.keys (query))) {
-          return this.buildURL (type.modelName, 'me', null, 'findRecord', null);
-        }
-
-        break;
+    if (signIn) {
+      url += '?login=true';
     }
 
-    return this._super (...arguments);
+    return url;
   },
 
-  headersForRequest (params) {
-    let {requestType, query, snapshot} = params;
-    let headers = this._super (...arguments);
+  urlForQueryRecord (query, modelName) {
+    return isEmpty (Object.keys (query)) ? this.buildURL (modelName, 'me', null, 'findRecord', null) : this._super (...arguments);
+  },
 
-    switch (requestType) {
-      case 'createRecord': {
-        const useUserToken = Ember.get (snapshot, 'adapterOptions.useUserToken');
-        const accessTokenKey = useUserToken ? 'session.accessToken' : 'session.gatekeeper.accessToken';
+  ajaxOptions (url /*, type, options*/) {
+    let hash = this._super (...arguments);
+    let beforeSend = hash.beforeSend || function (/*xhr*/) { };
 
-        // When creating an account record, we need to submit the client token, and not
-        // the user token. This is because we assume accounts are being created when the
-        // user is not signed in (i.e., by the client device).
-        const accessToken = this.get (accessTokenKey);
-        headers.Authorization = `Bearer ${accessToken.access_token}`;
-        break;
+    hash.beforeSend = function (xhr) {
+      // Call the original beforeSend() function.
+      beforeSend (xhr);
+
+      if (endsWith (url, '/me')) {
+        // For request about the current account, we do not want to cache the response.
+        // Otherwise, we run the risk of caching data related to a different user.
+        xhr.setRequestHeader ('Cache-Control', 'no-cache, no-store, must-revalidate');
       }
+    };
 
-      case 'queryRecord': {
-        if (Ember.isEmpty (Object.keys (query))) {
-          // The empty query object is shorthand for the current user without knowing the
-          // current user's id. We cannot cache this request, and must always go back to
-          // the server for the response to this request since the user could change.
-          headers['Cache-Control'] = 'no-cache, no-store, must-revalidate';
-        }
-        break;
-      }
-    }
-
-    return headers;
+    return hash;
   },
 
   handleResponse (status, headers, payload, requestData) {
-    let accessToken = payload.token;
+    if (status === 200 && requestData.method === 'POST') {
+      let accessToken = payload.token;
 
-    if (accessToken) {
-      delete payload.token;
-    }
+      if (isPresent (accessToken)) {
+        // Delete the access token from the payload.
+        delete payload.token;
 
-    if (status === 200 && requestData.method === 'POST' && accessToken) {
-      // The account was created and logged in at the same time. We need to
-      // extract the token, and register it with the gatekeeper service.
+        // The account was created and logged in at the same time. We need to
+        // extract the token, and register it with the gatekeeper service.
+        let session = this.get ('session');
+        let currentUser = {id: payload.account._id, username: payload.account.username, email: payload.account.email};
 
-      let session = this.get ('session');
-      let currentUser = {id: payload.account._id, username: payload.account.username, email: payload.account.email};
-
-      session.setProperties ({currentUser, accessToken});
+        session.setProperties ({currentUser, accessToken});
+      }
     }
 
     return this._super (status, headers, payload, requestData);
   }
 });
+
+//From http://stackoverflow.com/questions/280634/endswith-in-javascript
+function endsWith (string, suffix) {
+  if (typeof String.prototype.endsWith !== 'function') {
+    return string.indexOf(suffix, string.length - suffix.length) !== -1;
+  } else {
+    return string.endsWith(suffix);
+  }
+}
