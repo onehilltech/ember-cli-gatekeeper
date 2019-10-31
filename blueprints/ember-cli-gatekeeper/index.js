62d18ce69505beb7ae4d44b74092252ccb03e0a2
James Hill <james@onehilltech.com>
2019-10-31 00:36:56 -0500

    chore: binding ember-cli-mdc versions

--- blueprints/ember-cli-gatekeeper/index.js
+++ blueprints/ember-cli-gatekeeper/index.js
@@ -9,10 +9,10 @@ module.exports = Blueprint.extend ({
 
   addons: [
     {name: '@onehilltech/ember-cli-storage', target: '0.2.1'},
-    {name: 'ember-cli-mdc-form'},
-    {name: 'ember-cli-mdc-textfield'},
-    {name: 'ember-cli-mdc-button'},
-    {name: 'ember-cli-mdc-snackbar'},
+    {name: 'ember-cli-mdc-form', target: '0.80.0'},
+    {name: 'ember-cli-mdc-textfield', target: '0.80.2'},
+    {name: 'ember-cli-mdc-button', target: '0.80.0'},
+    {name: 'ember-cli-mdc-snackbar', target: '0.80.0'},
     {name: 'ember-copy'},
     {name: 'ember-cli-google-recaptcha', target: '2.3.1'},
     {name: 'ember-api-actions', target: '0.1.9'},
