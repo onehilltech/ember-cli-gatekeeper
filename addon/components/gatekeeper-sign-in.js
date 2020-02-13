153fd739b7e9611e338af27aa6c738a9d0166f93
James Hill <james@onehilltech.com>
2020-02-13 16:46:18 -0500

    fix: look for error message in message property

--- addon/components/gatekeeper-sign-in.js
+++ addon/components/gatekeeper-sign-in.js
@@ -118,7 +118,7 @@ export default Component.extend ({
       }
     }
     else {
-      this.setProperties ('errorMessage', xhr.statusText);
+      this.setProperties ('errorMessage', xhr.statusText || xhr.message);
     }
   },
 
