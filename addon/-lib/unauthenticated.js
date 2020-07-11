b10a8b1a1351ad8d32835997a6a3525b0664bbf7
James Hill <james@onehilltech.com>
2020-07-11 14:39:21 -0400

    feat: implemented unauthenticated as a decorator

---
+++ addon/-lib/unauthenticated.js
@@ -0,0 +1,35 @@
+import { get, getWithDefault } from '@ember/object';
+import { getOwner } from '@ember/application';
+
+import { Mixin as M } from 'base-object';
+
+const UnauthenticatedMixin = M.create ({
+  beforeModel () {
+    this._super (...arguments);
+    this._checkNotSignedIn ();
+  },
+
+  _checkNotSignedIn () {
+    let isSignedIn = get (this, 'session.isSignedIn');
+
+    if (isSignedIn) {
+      let ENV = getOwner (this).resolveRegistration ('config:environment');
+      let signInRoute = getWithDefault (ENV, 'gatekeeper.startRoute', 'index');
+
+      this.replaceWith (signInRoute);
+    }
+  },
+});
+
+function applyMixin (Clazz) {
+  return UnauthenticatedMixin.apply (Clazz.prototype);
+}
+
+export default function unauthenticated (param) {
+  if (isFunction (param)) {
+    return applyMixin (param);
+  }
+  else if (isPlainObject (param)) {
+    return applyMixin;
+  }
+}
