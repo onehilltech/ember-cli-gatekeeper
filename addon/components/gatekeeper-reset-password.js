aa04540667574a96cb489ffc8635e8433f8c7cc1
James Hill <james@onehilltech.com>
2021-03-28 18:53:34 -0400

    feat: added reset password component

---
+++ addon/components/gatekeeper-reset-password.js
@@ -0,0 +1,56 @@
+import Component from '@glimmer/component';
+import { tracked } from "@glimmer/tracking";
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { isPresent } from '@ember/utils';
+
+function noOp () { }
+
+export default class GatekeeperResetPasswordComponent extends Component {
+  @tracked
+  password;
+
+  @tracked
+  valid;
+
+  @service
+  gatekeeper;
+
+  @service
+  snackbar;
+
+  get passwordLabel () {
+    return this.args.passwordLabel || 'New password';
+  }
+
+  get submitButtonLabel () {
+    return this.args.submitButtonLabel || 'Reset password';
+  }
+
+  get submitButtonDisabled () {
+    return !this.valid;
+  }
+
+  get resetToken () {
+    return this.args.resetToken;
+  }
+
+  @action
+  validity (value) {
+    this.valid = value;
+  }
+
+  get reset () {
+    return this.args.reset || noOp;
+  }
+
+  @action
+  submit () {
+    this.gatekeeper.resetPassword (this.resetToken, this.password)
+      .then (() => this.reset ())
+      .catch (reason => {
+        const message = isPresent (reason.errors) ? reason.errors[0].detail : reason.message;
+        this.snackbar.show ( { message, dismiss: true });
+      });
+  }
+}
