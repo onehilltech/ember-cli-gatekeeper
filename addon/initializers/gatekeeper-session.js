cbefbaef7baee5af3a1cc59a6270b18273d4ba5e
James Hill <james@onehilltech.com>
2021-11-06 07:57:33 -0400

    feat: inject session into adapters

--- addon/initializers/gatekeeper-session.js
+++ addon/initializers/gatekeeper-session.js
@@ -1,6 +1,7 @@
 export function initialize (application) {
   application.inject ('route', 'session', 'service:session');
   application.inject ('controller', 'session', 'service:session');
+  application.inject ('adapter', 'session', 'service:session');
 }
 
 export default {
