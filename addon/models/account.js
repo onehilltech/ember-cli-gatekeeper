95a636717559eecbc267e3105aee63cbb304c618
James Hill <james@onehilltech.com>
2022-01-17 20:46:40 -0500

    fix: added resend() method to the account

--- addon/models/account.js
+++ addon/models/account.js
@@ -1,6 +1,7 @@
 import Model, { attr } from '@ember-data/model';
 import { memberAction } from 'ember-api-actions';
 import { fragment } from 'ember-data-model-fragments/attributes';
+import { serializeAndPush } from "ember-blueprint-data";
 
 export default class AccountModel extends Model {
   /// Email address for the account.
@@ -26,6 +27,16 @@ export default class AccountModel extends Model {
 
   /// Change the password for the account.
   changePassword = memberAction ({ path: 'password', type: 'post', urlType: 'findRecord'});
+
+  /**
+   * Resend the verification email.
+   */
+  resend = memberAction ({
+    path: 'resend',
+    type: 'post',
+    urlType: 'findRecord',
+    after: serializeAndPush ()
+  });
 
   /// The account has been verified.
   @attr('boolean')
