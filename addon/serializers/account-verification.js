7bdba27ecc2add48e9a88f37b0e440b07738a1f0
James Hill <james@onehilltech.com>
2022-01-25 01:10:48 -0500

    fix: added missing serializer for account-verification

---
+++ addon/serializers/account-verification.js
@@ -0,0 +1,20 @@
+import DS from 'ember-data';
+import { underscore } from '@ember/string';
+
+export default class AccountVerificationSerializer extends DS.RESTSerializer {
+  primaryKey = '_id';
+
+  keyForAttribute (key) {
+    return underscore (key);
+  }
+
+  serializeAttribute (snapshot, json, key, attributes) {
+    // Do not serialize any attributes that are null.
+    const attrs = snapshot.changedAttributes();
+    const changed = attrs[key];
+
+    if (changed && snapshot.attr (key)) {
+      super.serializeAttribute (snapshot, json, key, attributes);
+    }
+  }
+}
