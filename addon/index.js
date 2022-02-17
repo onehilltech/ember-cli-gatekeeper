867f54bb6edd605d0612524f6324759193711828
James Hill <james@onehilltech.com>
2022-02-16 22:00:37 -0500

    chore: export the token classes

--- addon/index.js
+++ addon/index.js
@@ -3,3 +3,5 @@ export { default as unauthenticated } from './-lib/unauthenticated';
 export { default as bearer } from './-lib/bearer';
 
 export { default as GatekeeperConfigurator, DefaultConfigurator as DefaultGatekeeperConfigurator } from './-lib/configurator';
+export { default as JWt } from './-lib/jwt';
+export { default as AccessToken } from './-lib/access-token';
