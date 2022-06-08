86d36e0e61a9e661dc9b31d63d4e93f7910cd177
James Hill <james@onehilltech.com>
2022-06-08 12:22:15 -0400

    fix: verified failed if user was signed out

--- addon/-lib/verified.js
+++ addon/-lib/verified.js
@@ -3,7 +3,7 @@ import decorator from '@onehilltech/decorator';
 
 function verified (target, name, descriptor, options = {}) {
   override.async (target.prototype, 'model', async function () {
-    if (this.session.account.verified) {
+    if (this.session.isSignedOut || this.session.account.verified) {
       return this._super.call (this, ...arguments);
     }
   });
